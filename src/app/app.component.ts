d1da553eaa463cc2b0dc365f780685a4df9c591c
igomarcelino <geslucian@hotmail.com>
2024-02-26 22:17:31 -0300

    First Tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {H1ComponentComponent} from './h1-component/h1-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,H1ComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/h1-component/h1-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-h1-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './h1-component.component.html',
+  styleUrl: './h1-component.component.css'
+})
+export class H1ComponentComponent {
+
+}

650686c4b384866c22fee954b70dbc73c7983ba3
igomarcelino <geslucian@hotmail.com>
2024-02-27 19:30:33 -0300

    add banner and photo galerry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './home-blog/home-blog.component';
+import {BannerHeaderComponent} from './components/banner-header/banner-header.component';
+import {HomeComponent} from './components/home-blog/home-blog.component';
+import {PhotoGalleryComponent} from './components/photo-gallery/photo-gallery.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent,BannerHeaderComponent,PhotoGalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/banner-header/banner-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner-header.component.html',
+  styleUrl: './banner-header.component.css'
+})
+export class BannerHeaderComponent {
+
+}
